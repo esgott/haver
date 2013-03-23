525a3e6400a768ec081a745a17bf77fe849b968b
Zsolt Szilagyi <esgott@gmail.com>
2013-03-23 19:21:31 +0100

    add calendar.php

--- start.php
+++ start.php
@@ -3,7 +3,7 @@
 elgg_register_page_handler('calendar', 'calendar_handler');
 
 function calendar_handler($segments) {
-	include elgg_get_plugins_path() . 'haver/pages/calendar.php';
+	include elgg_get_plugins_path() . 'haver/pages/haver/calendar.php';
 	return true;
 }
 
