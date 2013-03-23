b4f32345c53b3456769eaa63fa171973db2d5fb5
Zsolt Szilagyi <esgott@gmail.com>
2013-03-23 21:25:16 +0100

    process uploaded calendar file

--- actions/calendar_file_upload.php
+++ actions/calendar_file_upload.php
@@ -1,13 +1,25 @@
 <?php
 
 if (is_uploaded_file($_FILES['upload']['tmp_name'])) {
-	$filePointer = fopen($_FILES['upload']['tmp_name'], "rb");
+	$filePointer = fopen($_FILES['upload']['tmp_name'], 'rb');
+	$outputFile = fopen('/home/esgott/public_html/output.txt', 'w');
 	if ($filePointer != false){
+		$courses = array();
 		while (!feof($filePointer)) {
-			fgets($file);
+			$line = fgets($filePointer);
+			$match = preg_match('/^SUMMARY.*\(([a-zA-Z0-9]+)\).*$/', $line, $matches);
+			if ($match != false) {
+				array_push($courses, $matches[1]);
+			}
 		}
 		
-		fclose($file);
+		$courses = array_unique($courses);
+		foreach ($courses as $course) {
+			fwrite($outputFile, "$course\n");
+		}
+
+		fclose($filePointer);
+		fclose($outputFile);
 	}
 }
 
