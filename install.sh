e7d94947601020913cc8476402b60ad21d0a62c4
Zsolt Szilagyi <esgott@gmail.com>
2013-03-23 18:42:17 +0100

    add calendar uploader skeleton

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 SOURCE="/home/esgott/workspace/haver"
 TARGET="/home/esgott/public_html/elgg/mod/haver"
-COMPONENTS="manifest.xml start.php"
+COMPONENTS="pages manifest.xml start.php"
 
 mkdir -p $TARGET
 rm -rf $TARGET/*
 
 for component in $COMPONENTS; do
-	cp $SOURCE/$component $TARGET
+	cp -R $SOURCE/$component $TARGET
 done
 
 chgrp -R http $TARGET
--- start.php
+++ start.php
@@ -1,3 +1,10 @@
 <?php
 
+elgg_register_page_handler('calendar', 'calendar_handler');
+
+function calendar_handler($segments) {
+	include elgg_get_plugins_path() . 'haver/pages/calendar.php';
+	return true;
+}
+
 ?>
