a3c00010ea3c80ae8e003937cfdc1f723bdd8e57
Zsolt Szilagyi <esgott@gmail.com>
2013-03-23 17:48:53 +0100

    add empty start.php, update install.sh accordingly

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 SOURCE="/home/esgott/workspace/haver"
 TARGET="/home/esgott/public_html/elgg/mod/haver"
+COMPONENTS="manifest.xml start.php"
 
 rm -rf $TARGET/haver/*
 
-cp $SOURCE/manifest.xml $TARGET
+for component in $COMPONENTS; do
+	cp $SOURCE/$component $TARGET
+done
 
 chgrp -R http $TARGET
 chmod -R 775 $TARGET 
