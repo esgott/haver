69dc7eeaab80fbbd7e227c586887117ce0df69e0
Zsolt Szilagyi <esgott@gmail.com>
2013-03-30 10:35:01 +0100

    separate group name and id, join user to group

--- actions/calendar_file_upload.php
+++ actions/calendar_file_upload.php
@@ -1,6 +1,9 @@
 <?php
 
+global $log_file; //TODO remove log file
+
 $file = $_FILES['upload']['tmp_name'];
+
 $extension = end(explode(".", $_FILES["upload"]["name"]));
 $allowed_extensions = array("ics");
 $file_type = $_FILES['upload']['type'];
@@ -15,21 +18,20 @@ if (is_uploaded_file($file) &&
 	register_error(elgg_echo("Failed to upload file. Type: $file_type, size: $file_size, extension: $extension"));
 }
 
-// TODO remove output file
-
 function process_file($file) {
+	global $log_file;
+	$log_file = fopen('/home/esgott/public_html/output.txt', 'w');
 	$file_pointer = fopen($file, 'rb');
-	$output_file = fopen('/home/esgott/public_html/output.txt', 'w');
 	if ($file_pointer != false){
 
 		$courses = get_courses_from_file($file_pointer);
 
-		foreach ($courses as $course) {
-			fwrite($output_file, "$course\n");
-			create_group($course, $output_file);
+		foreach ($courses as $course_id => $course_name) {
+			fwrite($log_file, "$course_id => $course_name\n");
+			create_group($course_id, $course_name);
 		}
 
-		fclose($output_file);
+		fclose($log_file);
 		fclose($file_pointer);
 	}
 }
@@ -39,41 +41,58 @@ function get_courses_from_file($file_pointer) {
 
 	while (!feof($file_pointer)) {
 		$line = fgets($file_pointer);
-		$match = preg_match('/^SUMMARY.*\(([a-zA-Z0-9]+)\).*$/', $line, $matches);
+		$match = preg_match('/^SUMMARY:(.*) \(([a-zA-Z0-9]+)\).*$/', $line, $matches);
 		if ($match != false) {
-			array_push($courses, $matches[1]);
+			$course_id = $matches[2];
+			$course_name = $matches[1];
+			$courses[$course_id] = $course_name;
 		}
 	}
 
-	return array_unique($courses);
+	return $courses;
 }
 
-function create_group($name, $output_file) {
+function create_group($id, $name) {
+	global $log_file;
 	$options = array (
 			'metadata_names' => array('course'),
-			'metadata_values' => array($name)
+			'metadata_values' => array($id)
 	);
 	$group = elgg_get_entities_from_metadata($options);
 
 	if ($group) {
-		$guid = $group[0]->guid;
-		fwrite($output_file, "found: $guid\n");
+		$group = $group[0];
+		$guid = $group->guid;
+		fwrite($log_file, "found: $guid\n");
 	} else {
-		fwrite($output_file, "not found, creating new\n");
-		$group = new_group($name);
+		fwrite($log_file, "not found, creating new\n");
+		$group = new_group($id, $name);
 	}
+
+	join_user_to_group($group);
 }
 
-function new_group($name) {
+function new_group($id, $name) {
 	$group = new ElggGroup();
 	$group->subtype = 'haver_group';
 	$group->access_id = 1;
 	$group->name = $name;
-	$group->description = "Group for $name";
+	$group->description = $id;
 	$group->save();
-	$group->course = $name;
+	$group->course = $id;
 	$group->save();
 	return $group;
+}
+
+function join_user_to_group(ElggGroup $group) {
+	global $log_file;
+	$user = elgg_get_logged_in_user_entity();
+	$new_connection = $group->join($user);
+	if($new_connection) {
+		fwrite($log_file, "Was not a member\n");
+	} else {
+		fwrite($log_file, "Was already a member\n");
+	}
 }
 
 ?>
